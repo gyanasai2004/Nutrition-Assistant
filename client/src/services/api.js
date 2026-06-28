c54824026a1a5626e7903493a266f0fe22fc095e
gyanasai2004 <gyanasai.2@gmail.com>
2026-06-28 14:27:10 +0530

    update API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/users",
+  baseURL: "https://nutrition-assistant-9jpo.onrender.com",
 });
 
 export default API;
